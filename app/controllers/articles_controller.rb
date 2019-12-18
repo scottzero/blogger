df6e613373eda83914b3529601835a43d501bd7a
Scott Payton <spayton.c@gmail.com>
2019-12-17 19:47:30 -0700

    Create strong params in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,11 +13,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(
-       title: params[:article][:title],
-       body: params[:article][:body])
-     @article.save
-     redirect_to article_path(@article)
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 
 end
