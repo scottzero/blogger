f584a404b0cc8d1405b4256d4ea7f6fe800bb772
Scott Payton <spayton.c@gmail.com>
2019-12-18 20:26:36 -0700

    Passing unit test for tags and article many to many relationship

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,6 +1,6 @@
 describe Tag, type: :model do
     describe "relationships" do
-      it {should have_many(:tagings)}
+      it {should have_many(:taggings)}
       it {should have_many(:articles).through(:taggings)}
     end
   end
