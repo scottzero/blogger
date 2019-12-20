a3933abb02203c6681cc939b60be94698ba0a2c2
Scott Payton <spayton.c@gmail.com>
2019-12-19 20:20:55 -0700

    Fix tag list method in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@ class Article < ApplicationRecord
     end.join(", ")
   end
 
-  def tag_list=(tags_String)
+  def tag_list=(tags_string)
     tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
     #join tag strings on comma
     #strip to remove whitespace
