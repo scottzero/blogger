df024896bf8c6ff57b6d93bbe4be8c5ca9d21844
Scott Payton <spayton.c@gmail.com>
2019-12-19 20:13:47 -0700

    Create tag list methods to handle tag formatting

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,19 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_String)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    #join tag strings on comma
+    #strip to remove whitespace
+    #uniq to remove duplicates
+    #downcase to lowercase
+  end
+
 end
