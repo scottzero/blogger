1d92806c27a7b5227b3c6ac7cb099b3c80c1cd61
Scott Payton <spayton.c@gmail.com>
2019-12-21 12:27:10 -0700

    Update schema to handle file attachments on articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_031806) do
+ActiveRecord::Schema.define(version: 2019_12_21_191924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2019_12_19_031806) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
