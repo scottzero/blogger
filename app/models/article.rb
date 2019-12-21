b09fb99b348b07b434a207a6e85f4fc4b6add9db
Scott Payton <spayton.c@gmail.com>
2019-12-21 12:26:47 -0700

    Add validations for image atttributes in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  has_attached_file :image #has_attached_file is apart of the paper clip library
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 
   def tag_list
